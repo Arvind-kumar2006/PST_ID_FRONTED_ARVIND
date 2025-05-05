1faaed8cf25a7e431cdde268c214cc60263f08ea
Arvind-kumar214 <arvindkumar005152@gmail.com>
2025-05-05 12:54:44 +0530

    fronted

--- src/SignUp.jsx
+++ src/SignUp.jsx
@@ -1,6 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
 
 function SignUp() {
+  const [username , setUserName] = useState('');
+  const[email , setEmail] = useState('');
+  const[password , setPassword] = useState('');
+
+ const register = () =>{
+    const obj = {
+      username,
+      email,
+      password
+    }
+    try{
+      axios.post('http://localhost:8000/auth/register' ,obj)
+      alert("register succesfully")
+    }catch(error){
+      console.log(error.message)
+    }
+    }
   return (
     <>
     
@@ -10,10 +28,10 @@ function SignUp() {
         <br/>
         
         <form action="">
-              <input type="text" placeholder='username' style={{margin:'5px'}}/><br/>
-              <input type="email" placeholder='emai' style={{margin:'5px'}}/><br/>
-              <input type="password" placeholder='password' style={{margin:'5px'}} /><br/>
-              <button style={{margin:'10px'}}>Submit</button>
+              <input type="text" placeholder='username' style={{margin:'5px'}} value={username}    onChange={(e)=>setUserName(e.target.value)    }/><br/>
+              <input type="email" placeholder='emai' style={{margin:'5px'}} value={email}  onChange={(e)=>setEmail(e.target.value)}/><br/>
+              <input type="password" placeholder='password' style={{margin:'5px'}} value={password}  onChange={(e)=>setPassword(e.target.value)} /><br/>
+              <button style={{margin:'10px'}} onClick={register}>Submit</button>
           </form>
     </div>
    
